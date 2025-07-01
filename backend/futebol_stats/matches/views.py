1015eea06595c3917bbaf65695382cf1f4bac4e0
gabrielsoars <gabriel.s.mario@unesp.br>
2025-07-01 16:02:19 -0300

    fix app permissions

--- backend/futebol_stats/matches/views.py
+++ backend/futebol_stats/matches/views.py
@@ -1,8 +1,12 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from api_clients.api_football import get_matches_today, get_match_info, get_match_lineups
+from rest_framework.permissions import AllowAny, IsAuthenticated
+
 
 class MatchesTodayView(APIView):
+    permission_classes = [AllowAny]
+
     def get(self, request):
         # Descomente a linha abaixo para usar a API real
         # data = get_matches_today()
@@ -166,6 +170,8 @@ class MatchesTodayView(APIView):
         return Response(data)
     
 class MatchInfoView(APIView):
+    permission_classes = [AllowAny]
+
     def get(self, request, matchId):
         # data = get_match_info(matchId)
 
@@ -349,6 +355,8 @@ class MatchInfoView(APIView):
         return Response(data)
     
 class MatchLineupsView(APIView):
+    permission_classes = [AllowAny]
+
     def get(self, request, matchId):
         # data = get_match_lineups(matchId)
 
--- backend/futebol_stats/players/views.py
+++ backend/futebol_stats/players/views.py
@@ -2,8 +2,11 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from api_clients.api_football import get_player_info
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
 class PlayerDetailView(APIView):
+    permission_classes = [AllowAny]
+
     def get(self, request, player_id):
         # data = get_player_info(player_id)
 
